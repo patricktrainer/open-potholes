6d235dd7903806ef06cb24c6e46acf1387f6916c
Patrick <pat.trainer@gmail.com>
2021-08-25 01:50:50 -0500

    ic

---
+++ http.py
@@ -0,0 +1,21 @@
+from requests import get
+
+class DataNola:
+    def __init__(self) -> None:
+        pass
+
+    def connect(self):
+        pass
+
+    def get(self):
+        pass
+
+    def query(self):
+        pass
+
+
+
+
+
+
+
