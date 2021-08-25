98999fb1d55d2d1c044268c0c8df2d31e750828c
Patrick <pat.trainer@gmail.com>
2021-08-25 03:56:12 -0500

    make module

--- open_potholes/main.py
+++ open_potholes/main.py
@@ -1,10 +1,10 @@
-from http import NolaData
+from open_potholes import http
 
 if __name__ == "__main__":
     service_calls = "2jgv-pqrq"
     where = {"request_reason": "Pothole"}
 
-    data = NolaData(service_calls)
+    data = http.NolaData(service_calls)
     data.doFilter(where)
     response = data.doGet()
     
